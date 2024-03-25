0993f4b3a4eafa6af6c630949d396ffe089d4587
Dede-Husen <adehusen@gmail.com>
2024-03-25 15:47:42 +0700

    ubah on resume

--- app/src/main/java/com/pab/androidlifecycle/MainActivity.java
+++ app/src/main/java/com/pab/androidlifecycle/MainActivity.java
@@ -3,11 +3,15 @@ package com.pab.androidlifecycle;
 import static android.widget.Toast.LENGTH_LONG;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    String lifeCycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // bLOK CODE ONCREATE()
@@ -29,11 +33,20 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Toast.makeText(this, "Posisi lagi Resume  nih", LENGTH_LONG).show();
 
+
     }
 
     public void onPause() {
         super.onPause();
         Toast.makeText(this, "Posisi lagi pause  nih", LENGTH_LONG).show();
+        LinearLayout linearLayout = findViewById(R.id.linearParent);
+
+        Log.i(lifeCycle, "onPause() called");
+        // Ubah warna latar belakang LinearLayout
+        linearLayout.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        TextView txt1 = findViewById(R.id.txt1);
+        txt1.setText("POSISI LAGI PAUSE");
+        txt1.setTextColor(getColor(R.color.black));
 
     }
 
